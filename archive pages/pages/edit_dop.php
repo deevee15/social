1d440cc9b53a14c6b0f8eda7b67abdc9616e589c
deevee15 <vikulovd15@gmail.com>
2022-07-03 22:17:07 +0300

    Delete archive pages directory

--- archive pages/pages/edit_dop.php
+++
@@ -1,7 +0,0 @@
-<?
-    include_once("bd.php");
-    $about=$_POST['e_about'];
-    $query = "UPDATE users SET about='".$about."' WHERE email = '".$_COOKIE['account']."'";
-    $result = mysql_query($query) or die(mysql_error());;
-    echo "<meta http-equiv='Refresh' content='0; URL=/index.php'>";
-?>
